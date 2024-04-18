36c212352a0ea34e376492e3c84395380cc6fb1c
Ramesh Sannareddy <shrukulkarni.sk@gmail.com>
2024-04-18 11:34:48 -0400

    changes made from theia lab environment

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,10 +15,10 @@ urlpatterns = [
 
     #path for logout
     path(route='logout', view=views.logout, name='logout'),
-
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
     # path for dealer reviews view
 
     # path for add a review view
 
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
