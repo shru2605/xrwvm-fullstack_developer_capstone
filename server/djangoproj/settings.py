b6a349946973e92680ea3d540763afc51b774c62
Shruti <shrukulkarni.sk@gmail.com>
2024-04-24 01:41:21 -0400

    changes

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
         'USER': 'postgres',
-        'PASSWORD': 'NTA3OC1zaHJ1a3Vs',
+        'PASSWORD': 'MTMwMjYtc2hydWt1',
         'HOST': 'localhost',
         'PORT': '5432',
     }
