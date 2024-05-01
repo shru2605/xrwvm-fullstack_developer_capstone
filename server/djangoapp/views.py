591a0808803935daaffd39fa33cd57b7ee1f747c
shru2605 <71753117+shru2605@users.noreply.github.com>
2024-05-01 19:28:35 +0530

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -56,10 +56,10 @@ def registration(request):
     if not username_exist:
         # Create user in auth_user table
         user = User.objects.create_user(username=username,
-                        first_name=first_name,
-                        last_name=last_name,
-                        password=password,
-                        email=email)
+                                        first_name=first_name,
+                                        last_name=last_name,
+                                        password=password,
+                                        email=email)
         # Login the user and redirect to list page
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
