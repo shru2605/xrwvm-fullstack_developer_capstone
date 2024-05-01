c2f1f1bfb2bb220ea56cd53316967fc8f0a3e364
shru2605 <71753117+shru2605@users.noreply.github.com>
2024-05-01 14:32:36 +0530

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -20,7 +20,7 @@ class CarModel(models.Model):
         ('WAGON', 'Wagon'),
      ]
     type = models.CharField(max_length=10, 
-        choices = CAR_TYPES, 
+        choices=CAR_TYPES, 
         default='SUV')
     year = models.IntegerField(default=2023,
         validators = [
