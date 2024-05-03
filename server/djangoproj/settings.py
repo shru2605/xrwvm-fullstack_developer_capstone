b95cb19eb1ac402b161a1764c9b1ff1c97c044a7
shru2605 <71753117+shru2605@users.noreply.github.com>
2024-05-03 18:23:04 +0530

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',\
-                 'https://shrukulkarni-8000'\
-                 '.theiadockernext-0-labs-prod-theiak8s-4-tor01.'\
+                 'https://shrukulkarni-8000'
+                 '.theiadockernext-0-labs-prod-theiak8s-4-tor01.'
                  'proxy.cognitiveclass.ai']
 
 CSRF_TRUSTED_ORIGINS = [
